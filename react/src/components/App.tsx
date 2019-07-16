e78a4e777cd6ea8fc5c59a931faadbfb159de182
Nathan Hill <nateohill@gmail.com>
2019-07-16 13:12:43 -0700

    Added form to create ToDos Added interfaces for App Added i to state for temporary todo ids

--- react/src/components/App.tsx
+++ react/src/components/App.tsx
@@ -1,41 +1,146 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import styled from "styled-components";
 
-import ToDo from './ToDo';
+import ToDo from "./ToDo";
+
+import { reduxStore } from "../interfaces/reduxStore";
+import ACTIONS from "../redux/actions";
+
+const Form = styled.form`
+  display: flex;
+  flex-flow: column;
+  align-self: center;
+  height: 200px;
+  width: 200px;
+`;
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-self: center;
+  align-items: center;
+`;
+
+const TitleInput = styled.input`
+  flex-grow: 1;
+`;
+
+const ContentInput = styled.textarea`
+  flex-grow: 23;
+  resize: none;
+`;
+
+const SubmitButton = styled.input`
+  align-self: center;
+  width: fit-content;
+  height: fit-content;
+`;
+
+const mapStateToProps = (state: reduxStore) => ({
+  toDoList: state.toDoList
+});
+
+const mapDispatchToProps = (dispatch: Function) => ({
+  createItem: (title: string, content: string, id: number, isChecked: boolean, date: Date) =>
+    dispatch(ACTIONS.createItem(title, content, id, isChecked, date)),
+  deleteItem: (id: number) => dispatch(ACTIONS.deleteItem(id)),
+  updateItem: (id: number) => dispatch(ACTIONS.updateItem(id))
+});
+
+interface appProps {
+  createItem: Function;
+  deleteItem: Function;
+  updateItem: Function;
+  toDoList: any;
+}
 
 interface App {
-  generateToDos: Function,
-  handleSubmit: Function
+  props: appProps;
+  state: {
+    title: string;
+    content: string;
+    i: number
+  };
+  handleSubmit: Function;
+  handleChange: Function;
 }
 
 class App extends React.Component {
-  constructor(props: any) {
+  constructor(props: appProps) {
     super(props);
-    this.state = {};
-    this.generateToDos = (title: string, content: string, id: number) => {
-      return (
-        <ToDo attributes={{title, content, id}} key={id}/>
+    this.state = {
+      title: "",
+      content: "",
+      i: 0
+    };
+
+    this.handleSubmit = () => {
+      this.props.createItem(
+        this.state.title.length ? this.state.title : "ToDo",
+        this.state.content,
+        this.state.i,
+        false,
+        new Date()
       );
-    }
-    this.handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-      console.log(event);
-    }
+      this.setState({ title: "", content: "", i: this.state.i + 1});
+    };
+
+    this.handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      this.setState({
+        [event.target.name]: event.target.value
+      });
+    };
   }
 
   render() {
     return (
-      <div>
-        <form onSubmit={(event) => {
+      <AppContainer>
+        <Form
+          autoComplete="off"
+          onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
             event.preventDefault();
-            this.handleSubmit(event)
-          }
-        }>
-          <input type='text' name='title' />
-          <input type='text' name='content' />
-          <input type='submit' />
-        </form>
-      </div>
+            this.handleSubmit();
+          }}
+        >
+          <TitleInput
+            type="text"
+            name="title"
+            value={this.state.title}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              this.handleChange(event);
+            }}
+          />
+          <ContentInput
+            name="content"
+            value={this.state.content}
+            onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
+              this.handleChange(event);
+            }}
+          />
+          <SubmitButton type="submit" />
+        </Form>
+        {Object.keys(this.props.toDoList).map((toDo: string) => (
+          <ToDo updateItem={this.props.updateItem.bind(this)} deleteItem={this.props.deleteItem.bind(this)} attributes={{ ...this.props.toDoList[toDo] }} key={this.props.toDoList[toDo].id} />
+        ))}
+        {
+          console.log(fetch('/graphql', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+            },
+            body: JSON.stringify({query: "{ hello {id title content is_checked created_on}}"})
+          })
+            .then(r => r.json())
+            .then(data => console.log('data returned:', data)))
+        }
+      </AppContainer>
     );
   }
-};
+}
 
-export default App;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
