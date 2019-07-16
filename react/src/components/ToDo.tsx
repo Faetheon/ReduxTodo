e50f18d8fa7a95f1828a137715ff0830aeeb5519
Nathan Hill <nateohill@gmail.com>
2019-07-16 13:17:41 -0700

    Added Styled components to make it look nice. Added isChecked as a hook to track if a checkbox was checked

--- react/src/components/ToDo.tsx
+++ react/src/components/ToDo.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import { toDoItem } from '../interfaces/toDoItem';
 
-interface ToDo {
-  title: string,
-  content: string,
-  id: number
+const ToDoContainer = styled.div`
+  display: flex;
+  flex-flow: row;
+`;
 
-}
+const DeleteButton = styled.button`
+  flex-grow: 1;
+  color: red;
+  margin-left: 15px;
+`;
+
+const ToDoText = styled.div`
+  flex-grow: 2;
+`;
+
+const ToDoTitle = styled.div`
+  font-weight: bold;
+  font-size: 18px;
+  flex-shrink: 1;
+`;
+
+const ToDoContent = styled.div`
+  flex-grow: 1;
+`;
+
+const CheckBox = styled.input`
+  align-self: center;
+  margin-right: 15px;
+  flex-grow: 1;
+`;
+
 interface ToDoProps {
-  attributes: ToDo
+  attributes: toDoItem,
+  deleteItem: Function,
+  updateItem: Function
 }
 
 const ToDo = (props: ToDoProps) => {
-
+  const {title, content, id} = props.attributes;
+  const [isChecked, updateIsChecked] = useState<boolean>(props.attributes.isChecked);
   return (
-    <div>
-      {props.attributes.title}
-      <div>
-        {props.attributes.content}
-      </div>
-    </div>
+    <ToDoContainer>
+      <CheckBox type="checkbox" checked={isChecked} onChange={() => {props.updateItem(props.attributes.id); updateIsChecked(!isChecked)}} />
+      <ToDoText>
+        <ToDoTitle>
+          {title.slice(0, 1).toUpperCase() + title.slice(1)}
+        </ToDoTitle>
+        <ToDoContent>
+          {content}
+        </ToDoContent>
+      </ToDoText>
+      <DeleteButton onClick={(() => {props.deleteItem(id)})}>DELETE</DeleteButton>
+    </ToDoContainer>
   );
 };
 
