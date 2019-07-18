ed6fdcbdb4a5ebc8ff8d1a2e3e96255d32d9c6ac
Nathan Hill <nateohill@gmail.com>
2019-07-18 11:31:07 -0700

    Added updateItem to reducers.ts 	updateItem updates whether the item is checked Added populateState as an available action to get todos from the database on a interval Added fetch requests to each action to keep the database up to date with the state Changed all occurences of isChecked and createdOn to is_checked and created_on

--- react/src/redux/reducers.ts
+++ react/src/redux/reducers.ts
@@ -47,29 +47,87 @@ import { reduxStore } from '../interfaces/reduxStore';
 import ACTIONS from './actions';
 import { toDoItem } from '../interfaces/toDoItem';
 
-export const defaultState: reduxStore = {toDoList: {}};
+export const defaultState = { toDoList: {} };
 
 const reducer = (state = defaultState, action: any) => {
-  const newState: reduxStore = {toDoList: {}};
+  const newState: reduxStore = { toDoList: {} };
   Object.assign(newState.toDoList, state.toDoList);
-  switch(action.type) {
+  switch (action.type) {
     case ACTIONS.Types.CREATE_ITEM: {
       const toDo: toDoItem = {
         title: action.payload.title,
         content: action.payload.content,
-        id: action.payload.id,
-        isChecked: action.payload.isChecked,
-        createdOn: action.payload.createdOn
+        is_checked: action.payload.is_checked,
+        created_on: action.payload.created_on
       };
-      newState.toDoList[action.payload.id] = toDo;
+      (async function(title: String, content: String, is_checked: Boolean, created_on: string) {
+        let response = await fetch('/graphql', {
+          method: 'POST',
+          headers: {
+            "Content-Type":"application/json",
+            Accept:"application/json",
+          },
+          body: JSON.stringify({
+            query: `
+            {
+              createTodo(title: "${title}", content: "${content}", is_checked: ${is_checked}, created_on: "${created_on}") {
+                id
+              }
+            }
+            `
+          })
+        });
+        
+        let result = await response.json();
+        console.log(result, newState);
+          toDo.id = result.data.createTodo.id;
+          newState.toDoList[toDo.id] = toDo;
+        return newState;
+      })(toDo.title, toDo.content, toDo.is_checked, toDo.created_on);
+      return state;
+    }
+    case ACTIONS.Types.UPDATE_ITEM: {
+      newState.toDoList[action.payload].is_checked = !newState.toDoList[action.payload].is_checked;
+      fetch('/graphql', {
+        method: 'POST',
+        headers: {
+          "Content-Type":"application/json",
+          Accept:"application/json",
+        },
+        body: JSON.stringify({
+          query: `
+          {
+            updateTodo(id: "${action.payload}", is_checked: ${newState.toDoList[action.payload].is_checked})
+          }
+          `
+        })
+      })
+      .catch(err => {
+        console.error(err);
+      });
       return newState;
     }
     case ACTIONS.Types.DELETE_ITEM: {
       delete newState.toDoList[action.payload];
+      fetch('/graphql', {
+        method: 'POST',
+        headers: {
+          "Content-Type":"application/json",
+          Accept:"application/json",
+        },
+        body: JSON.stringify({
+          query: `
+          {
+            deleteTodo(id: "${action.payload}")
+          }
+          `
+        })
+      });
       return newState;
     }
-    case ACTIONS.Types.UPDATE_ITEM: {
-      newState.toDoList[action.payload].isChecked = !newState.toDoList[action.payload].isChecked;
+    case ACTIONS.Types.POPULATE_STATE: {
+      const {id, title, content, is_checked, created_on} = action.payload;
+      newState.toDoList[id] = {id, title, content, is_checked, created_on};
       return newState;
     }
     default:
